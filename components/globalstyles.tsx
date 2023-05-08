ae58de274248db0de69df6cbef6f1d60f1f01a98
lonr <lonr@users.noreply.github.com>
2023-05-08 23:19:06 +0800

    feat(globalstyles): add `scroll-padding-top` to `html`

--- components/globalstyles.tsx
+++ components/globalstyles.tsx
@@ -2,6 +2,16 @@ import { createGlobalStyle } from 'styled-components';
 import { themeGet } from '@primer/react';
 
 const GlobalStyle = createGlobalStyle`
+  html {
+    scroll-padding-top: 55px;
+  }
+
+  @media (max-width: ${themeGet('breakpoints.1')}) {
+    html {
+      scroll-padding-top: initial;
+    }
+  }
+
   body {
     overflow-y: overlay;
     background-color: ${themeGet('colors.canvas.default')};
