a6b96a2b9fcbbb3bb2930ab5d21c7eae3ae5794d
lonr <lonr@users.noreply.github.com>
2025-02-16 17:13:45 +0800

    refactor: migrate Tooltip to CSS Modules

--- components/Tooltip/Tooltip.tsx
+++ components/Tooltip/Tooltip.tsx
@@ -1,10 +1,9 @@
 // https://github.com/primer/react/blob/main/src/Tooltip.tsx
 
-import { sx, SxProp, themeGet } from '@primer/react';
-import { ComponentProps } from '@/types';
 import clsx from 'clsx';
-import { forwardRef, Ref } from 'react';
-import styled from 'styled-components';
+import { forwardRef, PropsWithChildren, Ref } from 'react';
+
+import classes from './Tooltip.module.css';
 
 export function tooltipLabelText(count: number) {
   if (count === 0) {
@@ -16,88 +15,27 @@ export function tooltipLabelText(count: number) {
   }
 }
 
-const TooltipBase = styled.div<SxProp>`
-  /* display: inline-block; */
-  text-decoration: none;
-  position: absolute;
-  z-index: 1000000;
-  padding: ${themeGet('space.2')} ${themeGet('space.3')};
-  font: normal normal 12px/1.5 ${themeGet('fonts.normal')};
-  -webkit-font-smoothing: subpixel-antialiased;
-  color: ${themeGet('colors.fg.onEmphasis')};
-  text-align: center;
-  text-decoration: none;
-  text-shadow: none;
-  text-transform: none;
-  letter-spacing: normal;
-  word-wrap: break-word;
-  white-space: pre;
-  pointer-events: none;
-  content: attr(aria-label);
-  background: ${themeGet('colors.neutral.emphasisPlus')};
-  border-radius: ${themeGet('radii.2')};
-
-  &::after {
-    display: inline-block;
-    text-decoration: none;
-    position: absolute;
-    z-index: 1000001;
-    width: 0px;
-    height: 0px;
-    color: ${themeGet('colors.neutral.emphasisPlus')};
-    pointer-events: none;
-    content: '';
-    border: 6px solid transparent;
-    border-top-color: ${themeGet('colors.neutral.emphasisPlus')};
-    bottom: -11px;
-  }
-
-  &.center {
-    transform: translate(-50%, calc(-100% - 6px));
-    &::after {
-      left: calc(50% - 6px);
-    }
-  }
-
-  &.left {
-    transform: translate(-15%, calc(-100% - 6px));
-    &::after {
-      left: calc(15% - 6px);
-    }
-  }
-
-  &.right {
-    transform: translate(-85%, calc(-100% - 6px));
-    /* transform: translateX(15%); */
-    &::after {
-      right: calc(15% - 6px);
-    }
-  }
-  ${sx};
-`;
-
-TooltipBase.displayName = 'TooltipBase (styled)';
-
-export type TooltipProps = {
+export type TooltipProps = PropsWithChildren<{
+  className?: string;
   direction?: 'center' | 'right' | 'left';
-} & ComponentProps<typeof TooltipBase>;
+}>;
 
 const Tooltip = forwardRef(
   (
     { direction = 'center', children, className, ...rest }: TooltipProps,
     ref: Ref<HTMLDivElement>,
   ) => {
-    const classes = clsx(className, direction);
     return (
-      <TooltipBase
+      <div
         role="tooltip"
         ref={ref}
         {...rest}
-        className={classes}
+        className={clsx(className, classes.TooltipBase)}
+        data-direction={direction}
         hidden
       >
         {children}
-      </TooltipBase>
+      </div>
     );
   },
 );
